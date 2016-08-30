8061c564d7d04f4a2a47784f0ac26e1ff81cc53e
RWoolard <r.woolard456@gmail.com>
2016-08-30 15:20:02 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -26,45 +26,53 @@ let b = 32
 */
 // write your code here
 
-
+print(a >= b)
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
 
-
+print(a % b == 0)
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
 
-
+print(y * b <= a)
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
 
 
-
+print(!(a >= b))
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
 
-
+if a % b == 0 {
+    print(true)
+}
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
 
 
-
+if (a / b) > Int(x) {
+    print("true")
+}
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
 
-
+if (y / Int(x)) > 3 {
+    print("true")
+} else {
+    print("false")
+}
 
 
 /*: Question 8
@@ -72,7 +80,9 @@ let b = 32
 */
 // write your code here
 
-
+if y > Int(x) && (a / b) > 9 {
+    print("true")
+}
 
 
 /*: Question 9
@@ -81,7 +91,13 @@ let b = 32
 // write your code here
 
 
-
+func isGreater(firstInt: Int, secondInt: Int) -> Bool {
+    if firstInt > secondInt{
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -90,17 +106,32 @@ let b = 32
 */
 // write your code here
 
-
-
+func isForceWith(name: String) -> Bool {
+    if (name == "Luke" || name == "Leia" || name == "Anakin" || name == "Obi Wan" || name == "Yoda" || name == "Vader"){
+        return true
+    } else {
+        return false
+    }
+}
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
 
+func goodBanker(firstAcct: Double, secondaryAcct: Double){
+    
+    
+    if secondaryAcct > 0.00 {
+      let newSecondaryAcct = secondaryAcct - 10.00
+      let newFirstAcct = firstAcct + 10.00
+       print(newFirstAcct)
+       print(newSecondaryAcct)
+    }
+    
+}
 
 
-
-
+goodBanker(30.00, secondaryAcct: 20.00)
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  */
